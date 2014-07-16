266f6e6509facbbb3f869d1757fffa4d5af10d79
©TriMoon™ <TriMoon@facebook.com>
2014-07-16 22:20:45 +0300

    Update FB-App Widescreen v0.2
    -Changed Copyright to CC 4.0
    -Changed namespace (claimid has ceased.)
    -Changed to run at document-start with needed changes.
    -Added icon from my FB page.
    -Preliminary fix incase FB changes the ID and the element can't be
    found.

--- FB-App Widescreen.user.js
+++ FB-App Widescreen.user.js
@@ -1,19 +1,35 @@
-// -*- Mode: Javascript; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 2 -*-
+﻿// -*- Mode: Javascript; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 2 -*-
 /*******************************************************************************
 	(C)2014+ TriMoon
-	Authors:	TriMoon <http://claimid.com/trimoon>
+	Authors:	TriMoon <https://www.facebook.com/TriMoon>
 
 	This work is licensed under a Creative Commons License !
 	See: http://creativecommons.org/licenses/by-nc-nd/4.0/
 *******************************************************************************/
 // ==UserScript==
 // @name       FB-App Widescreen
-// @namespace  http://claimid.com/trimoon
-// @version    0.1
+// @namespace  https://www.facebook.com/TriMoon
+// @version    0.2
 // @description  Make the apps become fullwide by hiding the right-side adverts bar
 // @match      https://apps.facebook.com/*
-// @copyright  2014+, TriMoon
+// @icon       https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/t1.0-1/p50x50/1234913_10203099716910993_2128955107_n.jpg
 // @author ©TriMoon™
+// @grant none
+// @run-at document-start
+// @copyright  http://creativecommons.org/licenses/by-nc-nd/4.0/
 // ==/UserScript==
 
-document.getElementById("rightCol").style.display="none";
+"use strict";
+(
+function (scope){
+	function MakeWide (){
+		var rightCol;
+		rightCol = scope.getElementById("rightCol");
+		if( rightCol ) rightCol.style.display="none";
+	}
+
+	if( scope.addEventListener ){
+		scope.addEventListener("DOMContentLoaded", MakeWide);
+	}
+}
+)( document );
